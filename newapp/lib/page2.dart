2b12a56b9c4f0f77ffeb8e03de2c0621eb9fb3a9
@Narayan78 <kandelnarayan82@gmail.com>
2022-01-24 16:06:11 +0545

    second commit

--- newapp/lib/page2.dart
+++ newapp/lib/page2.dart
@@ -10,50 +10,99 @@ class Page2 extends StatefulWidget {
 }
 
 class _Page2State extends State<Page2> {
+  get child => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('HomePage'),
-        ),
-        body: ListView(
-          children: const [
-            SizedBox(
-              height: 25,
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: ListView(
+        children: const [
+          LoginImage(),
+          SizedBox(
+            height: 25,
+          ),
+          Center(
+            child: Text(
+              'Login to Your Account ',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
             ),
-            Center(
-              child: Text(
-                'Login to Your Account ',
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 200,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(35.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter your Email Address',
+                prefixIcon: Icon(Icons.mail),
+              ),
+              textInputAction: TextInputAction.next,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Enter you Password',
+                prefixIcon: Icon(Icons.password),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 200,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(35.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Enter your Email Address',
-                  prefixIcon: Icon(Icons.mail),
-                ),
-                textInputAction: TextInputAction.next,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Enter you Password',
-                  prefixIcon: Icon(Icons.password),
-                ),
-              ),
-            )
-          ],
-        ));
+          ),
+          SizedBox(height: 10),
+          Button(),
+          SignupButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class Button extends StatefulWidget {
+  const Button({Key? key}) : super(key: key);
+
+  @override
+  _ButtonState createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Login'),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class SignupButton extends StatelessWidget {
+  const SignupButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text("Create a New Acccount"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class LoginImage extends StatelessWidget {
+  const LoginImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/pp.png");
   }
 }
